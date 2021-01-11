80e4fec156110548d78f5c9a1845600c2fd5f84e
ussii39 <usimarutetunori3@outlook.jp>
2021-01-12 02:38:02 +0900

    package.jsonの変更及び,reactの表示

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', include('frontend.urls'))
 ]
