cc0cad500cd245e4ef3ea49b7098bc7734c588ce
ussii39 <usimarutetunori3@outlook.jp>
2021-01-11 09:05:08 +0900

    py -m django startapp apiからsqliteの設定まで

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'ASIA/TOKYO'
 
 USE_I18N = True
 
