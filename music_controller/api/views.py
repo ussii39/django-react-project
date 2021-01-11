36a9bb6d3b10be13c9b46e310418bac22cfde40d
ussii39 <usimarutetunori3@outlook.jp>
2021-01-11 23:07:29 +0900

    sqlliteとmodelsのfixとfrontendfileの作成

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
 
 
-def main(request):
-    return HttpResponse("hello")
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    permission_classes = (AllowAny, )
