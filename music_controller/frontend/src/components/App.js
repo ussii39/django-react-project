dae13938b78f2fc48810166fe0b085f6d02e2f08
ussii39 <usimarutetunori3@outlook.jp>
2021-04-01 18:00:39 +0900

    fetch テスト

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,7 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import { render } from "react-dom";
 
 export const App = () => {
+  useEffect(() => {
+    return fetch("api/room", {
+      method: "GET",
+      // body: {},
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
   return <div>test</div>;
 };
 
